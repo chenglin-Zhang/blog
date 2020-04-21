38d9b8edbef4468bf4c664b43da3ddb0ecbcafd4
lin <zhangchenglinzcl@yahoo.com>
2020-04-21 12:44:10 +0800

    UPDATE BY Cheng 使用wsgi替换asgi BEGIN 20200421

--- dJan/asgi.py
+++ dJan/asgi.py
@@ -9,8 +9,16 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 
 import os
 
-from django.core.asgi import get_asgi_application
+'''
+    UPDATE BY Cheng 使用wsgi替换asgi BEGIN 20200421
+'''
+# from django.core.asgi import get_asgi_application
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dJan.settings')
 
-application = get_asgi_application()
+# application = get_asgi_application()
+application = get_wsgi_application()
+'''
+    UPDATE BY Cheng 使用wsgi替换asgi END 20200421
+'''
